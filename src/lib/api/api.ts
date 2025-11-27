b0a1280ee89162fc034ef853c19d3eea93c393bf
SaqibMehmood08 <inft212101008@gmail.com>
2025-11-28 01:40:41 +0500

    fixed URLs

--- src/lib/api/api.ts
+++ src/lib/api/api.ts
@@ -4,7 +4,7 @@ import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
  * Load environment variables
  */
 const APP_MODE = process.env.APP_MODE || "development";
-const BASE_URL = process.env.BACKEND_API_URL || "https://my.wavegames.online/api";
+const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_API_URL;
 
 /**
  * Create a single Axios instance
