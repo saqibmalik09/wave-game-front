4f90bc1c250e607aac95179cee104764e624f923
SaqibMehmood08 <inft212101008@gmail.com>
2026-01-19 09:31:59 +0500

    Greedy game setup added Do not delete branch

---
+++ src/app/games/greedy/GreedyGame.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import BetCategory from "./components/BetCategory";
+import BottomBar from "./components/BottomBar";
+import LeftPanel from "./components/LeftPanel";
+import RightPanel from "./components/RightPanel";
+import TopBar from "./components/TopBar";
+import Wheel from "./components/Wheel";
+
+
+
+export default function GreedyGame() {
+  return (
+    <div className="min-h-screen relative overflow-hidden  bg-gradient-to-b from-sky-300 to-blue-600">
+
+      {/* stars overlay */}
+      <div className="absolute inset-0 pointer-events-none opacity-60
+        [background-image:radial-gradient(2px_2px_at_20px_30px,white,transparent),
+        radial-gradient(2px_2px_at_40px_70px,rgba(255,255,255,0.8),transparent),
+        radial-gradient(2px_2px_at_50px_160px,white,transparent)]">
+      </div>
+
+      <div className="relative w-full min-h-screen max-w-full mx-auto">
+
+        <TopBar />
+        <LeftPanel />
+        <RightPanel />
+
+        <div className="absolute inset-0 flex items-center justify-center
+          px-[70px] pt-[60px] pb-[120px]">
+          <Wheel />
+        </div>
+
+        <BetCategory />
+        <BottomBar />
+
+      </div>
+    </div>
+  );
+}
